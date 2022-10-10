cc05425647282856b6d126baec7b3f9c8df0f49c
Nakuri <ccmuk58@naver.com>
2022-10-10 23:16:19 +0900

    [Fix] 조이스틱 개선 및 방향 표시

--- Assets/Script/JoyStick.cs
+++ Assets/Script/JoyStick.cs
@@ -6,48 +6,53 @@ namespace Script
 {
     public class JoyStick : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler
     {
-        public RectTransform rectBack;
-        public RectTransform rectJoystick;
-
-        public Vector3 joyDirection;
+        private RectTransform rectBack;
+        private RectTransform rectJoystick;
+        private Vector2 firstRectPos;
         private float backRadius;
-        Player player;
+        
+        public Vector2 joyDirection;
+        public bool isDrag;
  
         void Start()
         {
-            player = GameObject.FindWithTag("Player").GetComponent<Player>();
+            RectTransform[] rectTrans = GetComponentsInChildren<RectTransform>();
+            rectBack = rectTrans[1];
+            rectJoystick = rectTrans[2];
+            
             backRadius = rectBack.sizeDelta.y * 0.5f;
             backRadius *= transform.parent.GetComponent<RectTransform>().localScale.x;
-        }
- 
-        void Update()
-        {
-            player.Move(joyDirection);
+            
+            joyDirection = Vector2.up;
         }
  
         public void OnDrag(PointerEventData eventData)
         {
-            Vector3 pos = eventData.position;
-            Vector3 backPos = rectBack.transform.position;
+            Vector2 pos = eventData.position;
+            Vector2 backPos = firstRectPos;
             joyDirection = (pos - backPos).normalized;
- 
-            float distance = Vector3.Distance(pos, backPos);
-        
+            
+            float distance = Vector2.Distance(pos, backPos);
             if (distance < backRadius)
                 rectJoystick.position = backPos + joyDirection * distance;
             else
                 rectJoystick.position = backPos + joyDirection * backRadius;
+            
+            isDrag = true;
         }
  
         public void OnPointerDown(PointerEventData eventData)
         {
-            
+            firstRectPos = eventData.position;
+            rectBack.position = firstRectPos;
         }
  
         public void OnPointerUp(PointerEventData eventData)
         {
             rectJoystick.localPosition = Vector3.zero;
-            joyDirection = Vector3.zero;      
+            rectBack.anchoredPosition = Vector2.zero;
+            
+            isDrag = false;
         }
     }
 }
--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -5,32 +5,30 @@ namespace Script
     public class Player : MonoBehaviour
     {
         [Header("Player State")] 
-        [SerializeField]
-        private int maxHp = 100;
-        [SerializeField]
-        private int hp = 100;
-        [SerializeField]
-        public int power = 5;
-        [SerializeField]
-        private float speed = 3f;
-        [SerializeField]
-        private int xp = 0;
-    
-        void Update()
+        [SerializeField] private int maxHp = 100;
+        [SerializeField] private int hp = 100;
+        [SerializeField] public int power = 5;
+        [SerializeField] private float speed = 3f;
+        [SerializeField] private int xp = 0;
+
+        [SerializeField] private JoyStick joyStick;
+        [SerializeField] private Transform abilityHolderTrans;
+
+        private void Update()
         {
-            // Move(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            var dir = joyStick.joyDirection;
+            if(joyStick.isDrag)
+                Move(dir);
+            LookAt(dir);
         }
 
-        public void Move(float horizontal, float vertical)
+        private void LookAt(Vector2 lookVector)
         {
-            var moveValue = new Vector2
-            {
-                x = horizontal,
-                y = vertical
-            };
-            transform.Translate(moveValue * (speed * Time.deltaTime));
+            float rot = Mathf.Atan2(lookVector.y, lookVector.x) * Mathf.Rad2Deg;
+            abilityHolderTrans.rotation = Quaternion.Euler(0, 0, rot - 90);
         }
-        public void Move(Vector2 moveVector)
+
+        private void Move(Vector2 moveVector)
         {
             transform.Translate(moveVector * (speed * Time.deltaTime));
         }
@@ -38,8 +36,6 @@ namespace Script
         public void GetDamage(int damage)
         {
             hp -= damage;
-            Debug.Log($"DMG : " + damage);
-            Debug.Log($"HP : " + hp);
         }
     }
 }
