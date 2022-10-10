7f84f401f36448819c37fc3cfbc5d752cf012e72
Nakuri <ccmuk58@naver.com>
2022-10-11 04:06:15 +0900

    [Feat] 몬스터 스폰 로직 구현

---
+++ Assets/Script/EnemyHolder.cs
@@ -0,0 +1,38 @@
+﻿using Script.Skill;
+using UnityEngine;
+
+namespace Script
+{
+    public class EnemyHolder : MonoBehaviour
+    {
+        private void Start()
+        {
+            InvokeRepeating(nameof(SpawnEnemy), 0f, 0.25f);
+        }
+
+        private void SpawnEnemy()
+        {
+                Enemy enemy = ObjectPoolEnemy.GetObject();
+                enemy.transform.position = GetRandomPosition();
+        }
+        
+        public Vector3 GetRandomPosition()
+        {
+            float radius = 7f;
+            Vector3 playerPosition = transform.position;
+ 
+            float a = playerPosition.x;
+            float b = playerPosition.y;
+ 
+            float x = Random.Range(-radius + a, radius + a);
+            float y_b = Mathf.Sqrt(Mathf.Pow(radius, 2) - Mathf.Pow(x - a, 2));
+            y_b *= Random.Range(0, 2) == 0 ? -1 : 1;
+            float y = y_b + b;
+ 
+            Vector3 randomPosition = new Vector3(x, y, 0);
+            return randomPosition;
+        }
+
+    }
+    
+}
